b5a49f54d3e6832a7624ba534306914549446e86
ACB\anhnd08 <anhnd08@acb.com.vn>
2022-06-23 17:33:00 +0700

    initial commit

---
+++ broker-service/cmd/api/routes.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"net/http"
+)
+
+func routes() http.Handler {
+	mux := chi.NewRouter()
+
+	//specify who is allowed to connect
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"POST", "GET", "PUT", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Autorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: true,
+		ExposedHeaders:   []string{"Link"},
+		MaxAge:           300,
+	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
+	return mux
+}
